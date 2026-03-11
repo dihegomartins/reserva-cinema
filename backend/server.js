9674e4898f14e344740b91c8f9e605fa57f536fc
Dihêgo Pires <dihegopires@gmail.com>
2026-03-10 21:05:43 -0300

    refactor: organiza projeto em pastas backend e frontend (monorepo)

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('./'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend')))
 
 const dotenv = require('dotenv');
 const resultado = dotenv.config();
